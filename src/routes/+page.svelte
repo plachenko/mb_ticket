d70505e312bb7540cf2b19d7336d76b9ebacf42b
Denis Perchenko <plachenko@gmail.com>
2024-06-25 22:22:39 -0400

    adding basic animatiosn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,54 @@
 <script>
+  import { onMount } from "svelte";
+  import { fly } from "svelte/transition";
+
+  let arrNum = 0;
+  let deliArr = [
+    "ham",
+    "turkey",
+    "thin and trim",
+    "italian meats",
+    "chicken",
+    "bologna",
+  ];
+
+  let divInt = null;
+
+  onMount(() => {
+    divInt = setInterval((e) => {
+      if (arrNum >= deliArr.length - 1) {
+        clearInterval(divInt);
+        divInt = null;
+        return;
+      }
+      arrNum++;
+    }, 100);
+  });
 </script>
 
-<div class="p-2 flex flex-col gap-2">
-  <div class="bg-red-300 flex p-2">
-    <input class="w-full" type="text" />
+<div class="p-2 flex flex-col gap-2 h-full bg-gray-800">
+  <div class="flex p-2">
+    <input
+      class="w-full rounded-md p-2"
+      placeholder="search for a product..."
+      type="text"
+    />
   </div>
 
-  <div class="flex flex-1 bg-red-400 p-2">
-    <div class="grid grid-cols-4 gap-2">
-      {#each Array(10) as item}
-        <div class="bg-blue-400">test</div>
+  <div class="flex grow">
+    <div class="w-full grid grid-cols-4 gap-2">
+      {#each deliArr as item, idx}
+        <div class="relative">
+          {#if arrNum >= idx}
+            <div
+              transition:fly={{ y: 20 }}
+              class="bg-blue-400 absolute w-full rounded-md shadow flex h-full flex-col"
+            >
+              <div class="text-center">{item}</div>
+              <div></div>
+            </div>
+          {/if}
+        </div>
       {/each}
     </div>
   </div>
