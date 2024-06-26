e5bb071e64201b90eb78ff86e425b571c32b70a6
Denis Perchenko <plachenko@gmail.com>
2024-06-25 23:43:46 -0400

    adding images and product database

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,10 @@
 
   let divInt = null;
 
+  function test(){
+    console.log('hi')
+  }
+
   onMount(() => {
     divInt = setInterval((e) => {
       if (arrNum >= deliArr.length - 1) {
@@ -26,10 +30,11 @@
   });
 </script>
 
-<div class="p-2 flex flex-col gap-2 h-full bg-gray-800">
-  <div class="flex p-2">
+<div class="p-2 flex flex-col gap-2 h-full">
+  <div class="flex p-2 gap-4">
+    <img src="mbstacked.png" class="h-10" />
     <input
-      class="w-full rounded-md p-2"
+      class="w-full rounded-md p-2 border-2 border-slate-300"
       placeholder="search for a product..."
       type="text"
     />
@@ -38,11 +43,11 @@
   <div class="flex grow">
     <div class="w-full grid grid-cols-4 gap-2">
       {#each deliArr as item, idx}
-        <div class="relative">
+        <div class="relative cursor-pointer" onclick={test}>
           {#if arrNum >= idx}
             <div
               transition:fly={{ y: 20 }}
-              class="bg-blue-400 absolute w-full rounded-md shadow flex h-full flex-col"
+            class="bg-blue-400 absolute w-full rounded-md shadow flex h-full flex-col"
             >
               <div class="text-center">{item}</div>
               <div></div>
