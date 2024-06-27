b442414c6f5e015c902973124b8e193140cd26a8
Denis Perchenko <plachenko@gmail.com>
2024-06-26 21:49:47 -0400

    adding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,14 +13,15 @@
   ];
 
   let divInt = null;
-  let ticketNumber = null;
+  let ticketNumber = 1;
+  let ticketTaken = false;
 
-  function test(){
-    console.log('hi')
+  function test() {
+    console.log("hi");
   }
 
-  function setTicket(){
-    ticketNumber = 1;
+  function setTicket() {
+    ticketTaken = true;
     divInt = setInterval((e) => {
       if (arrNum >= deliArr.length - 1) {
         clearInterval(divInt);
@@ -31,49 +32,61 @@
     }, 100);
   }
 
-  onMount(() => {
-    
-  });
+  onMount(() => {});
 </script>
 
 <div class="p-2 flex flex-col gap-2 h-full">
-  {#if ticketNumber}
-  <div class="flex p-2 gap-4">
-    <img src="mbstacked.png" class="h-10" />
-    <input
-      class="w-full rounded-md p-2 border-2 border-slate-300"
-      placeholder="search for a product..."
-      type="text"
-    />
-    <div>
-      Ticket Number {ticketNumber}
+  {#if ticketTaken}
+    <div class="flex p-2 gap-4">
+      <img src="mbstacked.png" class="h-10" />
+      <input
+        class="w-full rounded-md p-2 border-2 border-slate-300"
+        placeholder="search for a product..."
+        type="text"
+      />
+      <div>
+        Ticket #{ticketNumber}
+      </div>
     </div>
-  </div>
 
-  <div class="flex grow">
-    <div class="w-full grid grid-cols-4 gap-2">
-      {#each deliArr as item, idx}
-        <div class="relative cursor-pointer" onclick={test}>
-          {#if arrNum >= idx}
-            <div
-              transition:fly={{ y: 20 }}
-            class="bg-blue-400 absolute w-full rounded-md shadow flex h-full flex-col"
-            >
-              <div class="text-center">{item}</div>
-              <div></div>
-            </div>
-          {/if}
-        </div>
-      {/each}
+    <div class="flex grow">
+      <div class="w-full grid grid-cols-4 gap-2">
+        {#each deliArr as item, idx}
+          <div class="relative cursor-pointer" onclick={test}>
+            {#if arrNum >= idx}
+              <div
+                transition:fly={{ y: 20 }}
+                class="bg-blue-400 absolute w-full rounded-md shadow flex h-full flex-col"
+              >
+                <div class="text-center">{item}</div>
+                <div></div>
+              </div>
+            {/if}
+          </div>
+        {/each}
+      </div>
     </div>
-  </div>
   {:else}
-  <div style="align-content:center" class="h-full w-full" onclick={setTicket} >
-    <div style="margin: 0 auto" class="w-[600px] text-center">
-      <img src="mbstacked.png" style="margin: 0px auto 30% auto" />
-      <div style="margin: 0 auto" class="animate-pulse p-4 border-4 border-red-300 font-bold text-red-300 rounded-lg text-[5em] text-center capitalize">please tap to get a ticket</div>
+    <div
+      style="align-content:center"
+      class="h-full w-full flex flex-row"
+      onclick={setTicket}
+    >
+      <div
+        style="margin: 0 auto"
+        class="flex-1 w-[45%] text-center gap-4 flex-col flex"
+      >
+        <div class="border-4 border-red-400 rounded-lg p-2">
+          <img src="mbstacked.png" style="margin: 0px auto" />
+        </div>
+        <div
+          style="margin: 0 auto"
+          class="animate-pulse p-4 border-4 border-red-300 shadow-md font-bold text-red-300 rounded-lg sm:text-[5em] text-center capitalize"
+        >
+          please tap to get ticket number {ticketNumber}
+        </div>
+      </div>
     </div>
-  </div>
   {/if}
 </div>
 
