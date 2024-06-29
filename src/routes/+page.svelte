bd9976ba4986c57e807e3b2817452ade0a001fbc
Denis Perchenko <plachenko@gmail.com>
2024-06-28 23:38:51 -0400

    adding cancel and fixing take ticket functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
   import { onMount } from "svelte";
   import { fly } from "svelte/transition";
-  import Nutritional from '$lib/nutritional.svelte';
+  import Nutritional from "$lib/nutritional.svelte";
 
-  let arrNum = 0;
+  let arrNum = -1;
   let deliArr = [
     "ham",
     "turkey",
@@ -16,6 +16,7 @@
   let divInt = null;
   let ticketNumber = 1;
   let ticketTaken = false;
+  let showCancel = false;
 
   function setTicket() {
     ticketTaken = true;
@@ -23,10 +24,17 @@
       if (arrNum >= deliArr.length - 1) {
         clearInterval(divInt);
         divInt = null;
+        showCancel = true;
         return;
       }
       arrNum++;
     }, 100);
+  }
+
+  function cancelTicket() {
+    showCancel = false;
+    ticketTaken = false;
+    arrNum = -1;
   }
 
   onMount(() => {});
@@ -47,15 +55,15 @@
     </div>
 
     <div class="flex grow">
-      <div class="w-full grid grid-cols-4 gap-2">
+      <div class="w-full grid grid-cols-4 gap-2 grow">
         {#each deliArr as item, idx}
-          <div class="relative cursor-pointer" onclick={showProducts}>
+          <div class="relative cursor-pointer">
             {#if arrNum >= idx}
               <div
                 transition:fly={{ y: 20 }}
                 class="bg-blue-400 absolute w-full rounded-md shadow flex h-full flex-col"
               >
-                <div class="text-center">{item}</div>
+                <div style="center-content" class="text-center">{item}</div>
                 <div></div>
               </div>
             {/if}
@@ -63,35 +71,43 @@
         {/each}
       </div>
     </div>
+    <div class="flex-1 shrink">
+      {#if showCancel}
+        <div
+          transition:fly={{ y: 20 }}
+          class="rounded-md bg-red-400 text-center p-4"
+          onclick={cancelTicket}
+        >
+          cancel
+        </div>
+      {/if}
+    </div>
   {:else}
     <div
       style="align-content: center"
-      class="h-full w-full"
+      class="relative h-full w-full"
       onclick={setTicket}
     >
-
-    <div
-      class="h-full w-full flex flex-row"
-    >
-      <div
-        style="margin: 0 auto"
-        class="flex-1 w-[45%] text-center gap-4 flex-col flex"
-      >
-        <div class="relative p-4 border-8 border-blue-500 rounded-lg">
-          <div class="absolute top-[-4px] left-[-4px] w-full border-red-400 h-full rounded-lg border-8">
-
-          </div>
-          <img src="mbstacked.png" style="margin: 0px auto" />
-        </div>
+      <div class="h-full w-full flex flex-row">
         <div
           style="margin: 0 auto"
-          class="animate-pulse p-4 border-4 border-red-300 shadow-md font-bold text-red-300 rounded-lg sm:text-[5em] text-center capitalize"
+          class="flex-1 w-[45%] text-center gap-4 flex-col flex"
         >
-          please tap to get ticket
+          <div
+            style="box-shadow: rgb(255, 0, 0) 0px 0px 0px 4px;"
+            class="relative p-4 border-4 border-blue-500 rounded-lg"
+          >
+            <img src="mbstacked.png" style="margin: 0px auto" />
+          </div>
+          <div
+            style="margin: 0 auto"
+            class="animate-pulse p-4 border-4 border-red-300 shadow-md font-bold text-red-300 rounded-lg sm:text-[5em] text-center capitalize"
+          >
+            please tap to get ticket
+          </div>
         </div>
       </div>
     </div>
-  </div>
   {/if}
 </div>
 
